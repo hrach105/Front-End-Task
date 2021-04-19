9deaba0e997d60bfbbc689354d72e43536bdfbf2
Hrach Khachatryan <hrachkhachatryan24@gmail.com>
2021-04-19 12:57:32 +0400

    home page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#open').click(function(){
+        $('.mobile-navbar').slideToggle()
+    })
+})
+
+$(function() {
+    $(".toggle-menu").click(function() {
+      $(this).toggleClass("active");
+    });
+  });
